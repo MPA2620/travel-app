9abde110ae27d9977e26126fb420e77494013add
Moldovan-Petraș Alexandru <moldovan-petrasalexandru@192-168-0-102.rdsnet.ro>
2024-05-21 17:18:43 +0300

    Fixed routing, added background and updated the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -8,7 +16,6 @@ import { ItineraryDetailsComponent } from './itinerary-details/itinerary-details
 import { RecommendationsComponent } from './recommendations/recommendations.component';
 import { SearchItinerariesComponent } from './search-itineraries/search-itineraries.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,8 +29,14 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
-    FormsModule 
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-itinerary/create-itinerary.component.ts
+++ src/app/create-itinerary/create-itinerary.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-itinerary',
   templateUrl: './create-itinerary.component.html',
-  styleUrl: './create-itinerary.component.css'
+  styleUrls: ['./create-itinerary.component.css']
 })
 export class CreateItineraryComponent {
+  itinerary = {
+    title: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    destinations: '',
+    activities: ''
+  };
+  submitted = false;
 
+  onSubmit() {
+    console.log('Itinerary Created:', this.itinerary);
+    this.submitted = true;
+  }
 }
